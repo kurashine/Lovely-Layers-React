84f8124e924abaf446e7a74e61c24d5a268ebc55
kurashine <dfgdgssd@gmail.com>
2026-03-23 13:42:37 +0200

    fix: home data structure and loading state

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 const baseURL = process.env.REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
-  baseURL,
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "ngrok-skip-browser-warning": "69420", // Этот заголовок отключает страницу-предупреждение
+  },
 });
 
 export const fetcher = (url: string) =>
